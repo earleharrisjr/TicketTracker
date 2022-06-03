7d3de5cf89ad5f430d35d0d15fcdfd933a601617
Earl Harris <earleharrisjr@gmail.com>
2022-06-03 14:38:38 -0400

    Working on Add Ticket

---
+++ TicketTracker/Controllers/AddTicketController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketTracker.Controllers
+{
+    public class AddTicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/addTicket/form")]
+        public IActionResult AddTicketForm()
+        {
+            return View("form");
+        }
+    }
+}
