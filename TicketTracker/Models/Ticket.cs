3f2a05aee1c3d001d68c9a70f75c58a325980c4c
Earl Harris <earleharrisjr@gmail.com>
2022-06-03 10:54:35 -0400

    worked on home/index view a little. Added Ticket model

---
+++ TicketTracker/Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketTracker.Models
+{
+    public class Ticket : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
