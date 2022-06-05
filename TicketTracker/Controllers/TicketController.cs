c60cb086705f42139013975f8822d9cc62dcb0c8
Earl Harris <earleharrisjr@gmail.com>
2022-06-05 18:50:29 -0400

    started to work on add feature/ works with viewbags right now.

---
+++ TicketTracker/Controllers/TicketController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketTracker.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/addTicket/form")]
+        public IActionResult AddTicketForm()
+        {
+            return View("AddTicketForm");
+        }
+        //handle post request that is getting sent by the form
+        [HttpPost]
+        [Route("/Adding")]
+        public IActionResult AddTicket(string issue, string details)
+        {
+            //passing form details into View("Home")
+            ViewBag.Issue = issue;
+            ViewBag.Details= details;   
+            return View("Index");
+        }
+
+
+    }
+}
