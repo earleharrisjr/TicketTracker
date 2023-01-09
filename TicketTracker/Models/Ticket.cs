2d7b193ccc0a8cc9b3882dfc322b6af12488d496
Earl Harris <earleharrisjr@gmail.com>
2023-01-09 13:12:42 -0500

    removed notes

--- TicketTracker/Models/Ticket.cs
+++ TicketTracker/Models/Ticket.cs
@@ -1,22 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace TicketTracker.Models
 {
     public class Ticket
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Issue { get; set; }
         public string Details { get; set; }
         public DateTime GetDate { get; set; }
 
+        //public List<NotesModel> Notes { get; set; }
+
 
         public Ticket() { }
+
         public Ticket(string issue, string details, DateTime getdate)
         {
             Issue = issue;
             Details = details;
-            GetDate = getdate;
+            GetDate = getdate;           
         }
     }
+
 }
